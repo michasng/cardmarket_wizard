5123f4bbbe7248db699f31e450ba8c363fe70f0e
Micha Sengotta <micha.sengotta@gmail.com>
2024-01-20 23:48:45 +0100

    Add shipping cost estimation

--- lib/services/cardmarket/shipping_costs_service.dart
+++ lib/services/cardmarket/shipping_costs_service.dart
@@ -2,9 +2,10 @@ import 'dart:convert';
 
 import 'package:cardmarket_wizard/models/enums/location.dart';
 import 'package:cardmarket_wizard/models/shipping_method.dart';
+import 'package:collection/collection.dart';
 import 'package:http/http.dart' as http;
 
-/// Based on API requests from https://help.cardmarket.com/en/ShippingCosts
+/// Based on info and API requests from https://help.cardmarket.com/en/ShippingCosts
 class ShippingCostsService {
   static ShippingCostsService? _instance;
 
@@ -12,6 +13,42 @@ class ShippingCostsService {
 
   factory ShippingCostsService.instance() {
     return _instance ??= ShippingCostsService._internal();
+  }
+
+  int _maxCardsInLetter(int maxletterWeightGram) {
+    return switch (maxletterWeightGram) {
+      20 => 4,
+      50 => 17,
+      100 => 40,
+      // approximation without source from cardmarket,
+      // verified that a more conservative factor of 0.2 is too low
+      _ => (0.4 * maxletterWeightGram).ceil(),
+    };
+  }
+
+  int estimateShippingCost({
+    required int cardCount,
+    required int valueEuroCents,
+    required List<ShippingMethod> shippingMethods,
+  }) {
+    // Cardmarket info is inaccurate. Tracking is required at (not over) 25,00 €.
+    // Theoretically also required for orders above 10 € from "new" sellers
+    // or orders from sellers with > 2 % or "too many" lost orders.
+    final mustUseTracking = valueEuroCents >= 2500;
+    final viableShippingMethods = shippingMethods.where(
+      (shippingMethod) =>
+          valueEuroCents <= shippingMethod.maxValueEuroCents &&
+          cardCount <= _maxCardsInLetter(shippingMethod.maxWeightGram) &&
+          (!mustUseTracking || shippingMethod.isTracked),
+    );
+    assert(viableShippingMethods.isNotEmpty, 'No suited shipping method.');
+
+    final cheapestShippingCost = viableShippingMethods
+        .map((shippingMethod) => shippingMethod.priceEuroCents)
+        .min;
+    final trusteeServiceCost =
+        mustUseTracking ? (valueEuroCents / 100).round() : 0;
+    return cheapestShippingCost + trusteeServiceCost;
   }
 
   Future<List<ShippingMethod>> findShippingMethods({
---
+++ test/services/cardmarket/shipping_costs_service_test.dart
@@ -0,0 +1,175 @@
+import 'package:cardmarket_wizard/models/shipping_method.dart';
+import 'package:cardmarket_wizard/services/cardmarket/shipping_costs_service.dart';
+import 'package:test/test.dart';
+
+final rawShippingMethods = [
+  {
+    'name': 'Standardbrief',
+    'isTracked': false,
+    'maxValue': '25,00 €',
+    'maxWeight': 20,
+    'stampPrice': '0,85 €',
+    'price': '1,15 €',
+    'isLetter': true,
+    'isVirtual': false
+  },
+  {
+    'name': 'Kompaktbrief',
+    'isTracked': false,
+    'maxValue': '25,00 €',
+    'maxWeight': 50,
+    'stampPrice': '1,00 €',
+    'price': '1,30 €',
+    'isLetter': true,
+    'isVirtual': false
+  },
+  {
+    'name': 'Grossbrief',
+    'isTracked': false,
+    'maxValue': '25,00 €',
+    'maxWeight': 500,
+    'stampPrice': '1,60 €',
+    'price': '2,10 €',
+    'isLetter': true,
+    'isVirtual': false
+  },
+  {
+    'name': 'Maxibrief',
+    'isTracked': false,
+    'maxValue': '25,00 €',
+    'maxWeight': 500,
+    'stampPrice': '2,75 €',
+    'price': '3,25 €',
+    'isLetter': true,
+    'isVirtual': false
+  },
+  {
+    'name': 'Kompaktbrief + PRIO',
+    'isTracked': true,
+    'maxValue': '100,00 €',
+    'maxWeight': 50,
+    'stampPrice': '2,10 €',
+    'price': '2,60 €',
+    'isLetter': true,
+    'isVirtual': false
+  },
+  {
+    'name': 'Kompaktbrief + EINSCHREIBEN Übergabe',
+    'isTracked': true,
+    'maxValue': '100,00 €',
+    'maxWeight': 50,
+    'stampPrice': '3,65 €',
+    'price': '4,15 €',
+    'isLetter': true,
+    'isVirtual': false
+  },
+  {
+    'name': 'Grossbrief + PRIO',
+    'isTracked': true,
+    'maxValue': '100,00 €',
+    'maxWeight': 500,
+    'stampPrice': '2,70 €',
+    'price': '3,20 €',
+    'isLetter': true,
+    'isVirtual': false
+  },
+  {
+    'name': 'Grossbrief + EINSCHREIBEN Übergabe',
+    'isTracked': true,
+    'maxValue': '100,00 €',
+    'maxWeight': 500,
+    'stampPrice': '4,25 €',
+    'price': '4,75 €',
+    'isLetter': true,
+    'isVirtual': false
+  },
+  {
+    'name': 'DHL Päckchen S',
+    'isTracked': false,
+    'maxValue': '25,00 €',
+    'maxWeight': 2000,
+    'stampPrice': '3,99 €',
+    'price': '4,99 €',
+    'isLetter': false,
+    'isVirtual': false
+  },
+  {
+    'name': 'DHL Päckchen M',
+    'isTracked': false,
+    'maxValue': '25,00 €',
+    'maxWeight': 2000,
+    'stampPrice': '4,79 €',
+    'price': '5,79 €',
+    'isLetter': false,
+    'isVirtual': false
+  },
+];
+
+void main() {
+  final shippingMethods = [
+    for (final method in rawShippingMethods) ShippingMethod.fromJson(method),
+  ];
+  final shippingCostsService = ShippingCostsService.instance();
+
+  group('estimateShippingCost', () {
+    for (int cardCount in [1, 4]) {
+      test('$cardCount cheap card(s)', () {
+        final result = shippingCostsService.estimateShippingCost(
+          cardCount: cardCount,
+          valueEuroCents: 10 * cardCount,
+          shippingMethods: shippingMethods,
+        );
+        expect(result, 115);
+      });
+    }
+
+    for (int cardCount in [5, 17]) {
+      test('$cardCount cheap cards', () {
+        final result = shippingCostsService.estimateShippingCost(
+          cardCount: cardCount,
+          valueEuroCents: 10 * cardCount,
+          shippingMethods: shippingMethods,
+        );
+        expect(result, 130);
+      });
+    }
+
+    for (int cardCount in [18, 100, 200]) {
+      test('$cardCount cheap cards', () {
+        final result = shippingCostsService.estimateShippingCost(
+          cardCount: cardCount,
+          valueEuroCents: 10 * cardCount,
+          shippingMethods: shippingMethods,
+        );
+        expect(result, 210);
+      });
+    }
+
+    test('201 cheap cards', () {
+      final result = shippingCostsService.estimateShippingCost(
+        cardCount: 201,
+        valueEuroCents: 2010,
+        shippingMethods: shippingMethods,
+      );
+      expect(result, 499);
+    });
+
+    test('1 expensive card', () {
+      final result = shippingCostsService.estimateShippingCost(
+        cardCount: 1,
+        valueEuroCents: 2500,
+        shippingMethods: shippingMethods,
+      );
+      expect(result, 285);
+    });
+
+    test('2 expensive cards', () {
+      final result = shippingCostsService.estimateShippingCost(
+        cardCount: 2,
+        valueEuroCents: 5000,
+        shippingMethods: shippingMethods,
+      );
+      expect(result, 310);
+    });
+  });
+}
