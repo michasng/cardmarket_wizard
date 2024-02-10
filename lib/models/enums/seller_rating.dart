3c08e91bb87d4ba966226655e2f096466386e6bc
Micha Sengotta <micha.sengotta@gmail.com>
2024-02-10 20:09:46 +0100

    Make SellerRating comparable

--- lib/models/enums/seller_rating.dart
+++ lib/models/enums/seller_rating.dart
@@ -1,10 +1,21 @@
-enum SellerRating {
-  outstanding('Outstanding'),
-  veryGood('Very Good'),
-  good('Good'),
-  bad('Bad');
+enum SellerRating implements Comparable<SellerRating> {
+  outstanding('Outstanding', 3),
+  veryGood('Very Good', 2),
+  good('Good', 1),
+  bad('Bad', 0);
 
   final String label;
+  final int ordinal;
 
-  const SellerRating(this.label);
+  const SellerRating(this.label, this.ordinal);
+
+  @override
+  int compareTo(SellerRating other) {
+    return ordinal.compareTo(other.ordinal);
+  }
+
+  operator >(SellerRating other) => ordinal > other.ordinal;
+  operator >=(SellerRating other) => ordinal >= other.ordinal;
+  operator <(SellerRating other) => ordinal < other.ordinal;
+  operator <=(SellerRating other) => ordinal <= other.ordinal;
 }
