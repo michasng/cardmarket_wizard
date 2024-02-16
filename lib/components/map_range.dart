18a992ff183a3db194ecbc9b8140156b7ecc2c87
Micha Sengotta <micha.sengotta@gmail.com>
2024-02-16 17:47:45 +0100

    Add documentation to map range functions

--- lib/components/map_range.dart
+++ lib/components/map_range.dart
@@ -1,6 +1,10 @@
+/// A range of [T], from lower to upper.
 typedef Range<T extends num> = ({T lower, T upper});
 
 extension MapRange<T extends num> on T {
+  /// Maps this [T] from a starting range [from] to a target range [to].
+  /// Unlike [mapIntRange], the result will be [double].
+  /// This must not necessarily lie within either of those ranges.
   double mapRange({required Range<T> from, required Range<T> to}) {
     return (this - from.lower) *
             (to.upper - to.lower) /
@@ -10,6 +14,9 @@ extension MapRange<T extends num> on T {
 }
 
 extension MapIntRange on int {
+  /// Maps this [int] from a starting range [from] to a target range [to].
+  /// Unlike [mapRange], the result will remain [int], at a cost of accuracy depending on the use-case.
+  /// This must not necessarily lie within either of those ranges.
   int mapIntRange({required Range<int> from, required Range<int> to}) {
     return (this - from.lower) *
             (to.upper - to.lower) ~/
