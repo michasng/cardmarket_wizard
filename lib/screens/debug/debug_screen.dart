e9ab4d188bd078b587c074d096068296073bf141
Micha Sengotta <micha.sengotta@gmail.com>
2025-02-25 21:20:39 +0100

    Add a debug option to print cookies

--- lib/screens/debug/debug_screen.dart
+++ lib/screens/debug/debug_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:cardmarket_wizard/components/location_dropdown.dart';
 import 'package:cardmarket_wizard/models/enums/location.dart';
 import 'package:cardmarket_wizard/navigator_state_go.dart';
 import 'package:cardmarket_wizard/screens/launch/launch_screen.dart';
+import 'package:cardmarket_wizard/services/browser_holder.dart';
 import 'package:cardmarket_wizard/services/cardmarket/pages/card_page.dart';
 import 'package:cardmarket_wizard/services/cardmarket/pages/seller_singles_page.dart';
 import 'package:cardmarket_wizard/services/cardmarket/pages/single_page.dart';
@@ -26,118 +29,154 @@ class _DebugScreenState extends State<DebugScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Debugging options',
-              style: Theme.of(context).textTheme.titleLarge,
+      body: SelectionArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                spacing: 16,
+                children: [
+                  Text(
+                    'Debugging options',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  Text(
+                    'These actions log to the console.',
+                  ),
+                  FilledButton(
+                    onPressed: () async {
+                      try {
+                        final browserHolder = BrowserHolder.instance();
+                        final page = await browserHolder.currentPage;
+                        final cookies = await page.cookies();
+                        final cookiesSerializable =
+                            cookies.map((cookie) => cookie.toJson()).toList();
+                        DebugScreen._logger.info(
+                          'COOKIES: ${jsonEncode(cookiesSerializable)}',
+                        );
+                      } catch (e) {
+                        DebugScreen._logger.severe(e);
+                        rethrow;
+                      }
+                    },
+                    child: const Text('Print cookies'),
+                  ),
+                  FilledButton(
+                    onPressed: () async {
+                      try {
+                        final page = await WantsPage.fromCurrentPage();
+                        final wants = await page.parse();
+                        DebugScreen._logger.info('PARSED WANTS: $wants');
+                      } catch (e) {
+                        DebugScreen._logger.severe(e);
+                        rethrow;
+                      }
+                    },
+                    child: const Text('Force parse wants'),
+                  ),
+                  FilledButton(
+                    onPressed: () async {
+                      try {
+                        final page = await CardPage.fromCurrentPage();
+                        final card = await page.parse();
+                        DebugScreen._logger.info('PARSED CARD: $card');
+                      } catch (e) {
+                        DebugScreen._logger.severe(e);
+                        rethrow;
+                      }
+                    },
+                    child: const Text('Force parse card'),
+                  ),
+                  FilledButton(
+                    onPressed: () async {
+                      try {
+                        final page = await SinglePage.fromCurrentPage();
+                        final single = await page.parse();
+                        DebugScreen._logger.info('PARSED SINGLE: $single');
+                      } catch (e) {
+                        DebugScreen._logger.severe(e);
+                        rethrow;
+                      }
+                    },
+                    child: const Text('Force parse single'),
+                  ),
+                  FilledButton(
+                    onPressed: () async {
+                      try {
+                        final page = await SellerSinglesPage.fromCurrentPage();
+                        final sellerSingles = await page.parse();
+                        DebugScreen._logger
+                            .info('PARSED SELLER SINGLES: $sellerSingles');
+                      } catch (e) {
+                        DebugScreen._logger.severe(e);
+                        rethrow;
+                      }
+                    },
+                    child: const Text('Force parse seller singles'),
+                  ),
+                  Row(
+                    spacing: 16,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        child: LocationDropdown(
+                          labelText: 'from',
+                          value: _fromCountry,
+                          onChanged: (newValue) {
+                            setState(() {
+                              _fromCountry = newValue;
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        width: 200,
+                        child: LocationDropdown(
+                          labelText: 'to',
+                          value: _toCountry,
+                          onChanged: (newValue) {
+                            setState(() {
+                              _toCountry = newValue;
+                            });
+                          },
+                        ),
+                      ),
+                      FilledButton(
+                        onPressed: () async {
+                          try {
+                            final shippingCostsService =
+                                ShippingCostsService.instance();
+                            final shippingCosts =
+                                await shippingCostsService.findShippingMethods(
+                              fromCountry: _fromCountry,
+                              toCountry: _toCountry,
+                            );
+                            DebugScreen._logger.info(
+                              'SHIPPING COSTS: $shippingCosts',
+                            );
+                          } catch (e) {
+                            DebugScreen._logger.severe(e);
+                            rethrow;
+                          }
+                        },
+                        child: const Text('Determine shipping costs'),
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      final navigator = Navigator.of(context);
+                      navigator.go(const LaunchScreen());
+                    },
+                    child: const Text('Restart wizard'),
+                  ),
+                ],
+              ),
             ),
-            FilledButton(
-              onPressed: () async {
-                try {
-                  final page = await WantsPage.fromCurrentPage();
-                  final wants = await page.parse();
-                  DebugScreen._logger.info('PARSED WANTS: $wants');
-                } catch (e) {
-                  DebugScreen._logger.severe(e);
-                  rethrow;
-                }
-              },
-              child: const Text('Force parse wants'),
-            ),
-            FilledButton(
-              onPressed: () async {
-                try {
-                  final page = await CardPage.fromCurrentPage();
-                  final card = await page.parse();
-                  DebugScreen._logger.info('PARSED CARD: $card');
-                } catch (e) {
-                  DebugScreen._logger.severe(e);
-                  rethrow;
-                }
-              },
-              child: const Text('Force parse card'),
-            ),
-            FilledButton(
-              onPressed: () async {
-                try {
-                  final page = await SinglePage.fromCurrentPage();
-                  final single = await page.parse();
-                  DebugScreen._logger.info('PARSED SINGLE: $single');
-                } catch (e) {
-                  DebugScreen._logger.severe(e);
-                  rethrow;
-                }
-              },
-              child: const Text('Force parse single'),
-            ),
-            FilledButton(
-              onPressed: () async {
-                try {
-                  final page = await SellerSinglesPage.fromCurrentPage();
-                  final sellerSingles = await page.parse();
-                  DebugScreen._logger
-                      .info('PARSED SELLER SINGLES: $sellerSingles');
-                } catch (e) {
-                  DebugScreen._logger.severe(e);
-                  rethrow;
-                }
-              },
-              child: const Text('Force parse seller singles'),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                LocationDropdown(
-                  labelText: 'from',
-                  value: _fromCountry,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _fromCountry = newValue;
-                    });
-                  },
-                ),
-                LocationDropdown(
-                  labelText: 'to',
-                  value: _toCountry,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _toCountry = newValue;
-                    });
-                  },
-                ),
-                FilledButton(
-                  onPressed: () async {
-                    try {
-                      final shippingCostsService =
-                          ShippingCostsService.instance();
-                      final shippingCosts =
-                          await shippingCostsService.findShippingMethods(
-                        fromCountry: _fromCountry,
-                        toCountry: _toCountry,
-                      );
-                      DebugScreen._logger.info(
-                        'SHIPPING COSTS: $shippingCosts',
-                      );
-                    } catch (e) {
-                      DebugScreen._logger.severe(e);
-                      rethrow;
-                    }
-                  },
-                  child: const Text('Find some shipping costs'),
-                ),
-              ].separated(const Gap()),
-            ),
-            TextButton(
-              onPressed: () {
-                final navigator = Navigator.of(context);
-                navigator.go(const LaunchScreen());
-              },
-              child: const Text('Restart wizard'),
-            ),
-          ].separated(const Gap()),
+          ),
         ),
       ),
     );
